b193d0989c03334a8bc973b798848de039196695
Firdavs Yuldashev <firdavsyuldashev111@gmai.com>
2025-09-01 07:52:20 +0500

    fixed typescript errors

--- src/components/tasks/kanban/item.tsx
+++ src/components/tasks/kanban/item.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 interface Props {
     id: string, 
-    data?: UseDraggableArguments
+    data?: UseDraggableArguments["data"]
 }
 
 export const KanbanItem = ({ children, id, data }: React.PropsWithChildren<Props>) => {
